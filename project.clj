c271b38ef38dc84ea492bb161137b65b0f96f49f
Roman Rudakov <rrudakov@protonmail.com>
2021-03-10 20:57:07 +0100

    Format project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject telegram-chat-bot "0.3.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Telegram chat bot for my personal needs"
+  :url "https://github.com/rrudakov/telegram-chat-bot"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [ring/ring-jetty-adapter "1.7.1"]
                  [metosin/reitit-core "0.5.11"]
                  [metosin/reitit-ring "0.5.11"]
@@ -30,4 +30,4 @@
             [lein-cljfmt "0.7.0"]]
   :cljfmt {}
   :ring {:handler telegram-chat-bot.core/app
-         :port 3001})
+         :port    3001})
